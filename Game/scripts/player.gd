85c16fe5c2ea8d5503d09917921ca9763a525e23
Ocskó Gábor <ogabor@gmail.com>
2025-10-16 12:57:55 +0200

    Refactor player.gd to use a constant for gravity instead of project settings, improving consistency in gravity application during physics processing.

--- Game/scripts/player.gd
+++ Game/scripts/player.gd
@@ -3,20 +3,21 @@ extends CharacterBody2D
 
 const SPEED = 180.0
 const JUMP_VELOCITY = -450.0
-# const GRAVITY = 1800.0 # 1800 pixels per second squared
+const GRAVITY = 1800.0 # 1800 pixels per second squared
 
 
 @onready var player = $"."
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+# var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		# velocity.y += gravity * delta
+		velocity.y += GRAVITY * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
