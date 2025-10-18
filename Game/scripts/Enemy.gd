be7957deecb97759a5f2e015a48f557916df0b55
Ocskó Gábor <ogabor@gmail.com>
2025-10-18 23:06:45 +0200

    Add attack animation and player detection to enemy character. Update enemy.tscn to include attack animation frames and an Area2D for player detection. Modify Enemy.gd to handle attack state and trigger animations accordingly, enhancing enemy interaction with the player.

--- Game/scripts/Enemy.gd
+++ Game/scripts/Enemy.gd
@@ -10,6 +10,7 @@ var direction = -1
 
 var currentHealth = 100
 var isDead = false
+var isAttacking = false
 
 func _process(_delta):
 	UpdateAnimation()
@@ -20,6 +21,12 @@ func _physics_process(_delta):
 
 	if isDead:
 		return
+
+	if isAttacking:
+		if not animated_sprite_2d.is_playing():
+			isAttacking = false
+		else:
+			return
 
 	if ray_cast_2d_forward.is_colliding() || ray_cast_2d_downward.is_colliding() == false:
 		direction = - direction
@@ -37,7 +44,11 @@ func UpdateAnimation():
 	if velocity.x != 0:
 		animated_sprite_2d.flip_h = velocity.x > 0
 
-	animated_sprite_2d.play("walk")
+
+	if isAttacking == false:
+		animated_sprite_2d.play("walk")
+	elif animated_sprite_2d.animation != "attack":
+		animated_sprite_2d.play("attack")
 
 func ApplyDamage(damage: int):
 	if isDead:
@@ -50,3 +61,7 @@ func ApplyDamage(damage: int):
 		set_collision_layer_value(3, false)
 		await get_tree().create_timer(2).timeout
 		queue_free()
+
+
+func _on_area_2d_player_detector_body_entered(_body):
+	isAttacking = true
